064126a2599785a650a5ab0fc15dcc65dccf4ac1
OJ167 <olliejackson@hotmail.co.uk>
2023-07-21 09:28:25 +0100

    late night

--- Code/Python/Useful Code/Validation/Vorticity_Validation.py
+++ Code/Python/Useful Code/Validation/Vorticity_Validation.py
@@ -13,17 +13,17 @@ import matplotlib.cm
 # from colorspacious import cspace_converter
 
 #####Import Ollie Tools
-dirPath = "C:/Coding"
-sys.path.insert(0, dirPath)
-import OllieTools as oj
-print(dirPath)
-
-
-####Import Ollie Tools MAC
-# dirPath = "/Users/olliejackson/Coding"
+# dirPath = "C:/Coding"
 # sys.path.insert(0, dirPath)
 # import OllieTools as oj
 # print(dirPath)
+
+
+####Import Ollie Tools MAC
+dirPath = "/Users/olliejackson/Coding"
+sys.path.insert(0, dirPath)
+import OllieTools as oj
+print(dirPath)
 
 ##### Set plot style #####
 plt.style.use(["science", "vibrant", "no-latex"])
@@ -46,15 +46,15 @@ cmap = plt.get_cmap("jet_r")
 
 
 #### Simple Vortex
-vf_x = lambda x, y: y
-vf_y = lambda x, y: -x
+# vf_x = lambda x, y: y
+# vf_y = lambda x, y: -x
 # Stream function = 1/2y^2+1/2x^2
 
 
 
 
-# vf_x = lambda x, y: 4 * y
-# vf_y = lambda x, y: -x
+vf_x = lambda x, y: 10 * y
+vf_y = lambda x, y: -x
 # Stream function = (2*Y**2)+(0.5*X**2))
 
 
@@ -75,7 +75,7 @@ for i in range(X.shape[0]):
           
 fig, ax = plt.subplots(sharex=True, sharey=True)
 ax.quiver(X, Y, U, V, units='xy', pivot = 'middle', scale=scale)
-ax.contour((0.5*(Y**2))+(0.5*(X**2)))
+ax.contour((5*(Y**2))+(0.5*(X**2)))
 
 # plt.show()
 
@@ -191,10 +191,11 @@ point_d = [75, 25]
 
 f2, ax2 = plt.subplots()
 ax2.contourf(vort)
-ax2.scatter(point_a[1],point_a[0]) 
-ax2.scatter(point_b[1],point_b[0])
-ax2.scatter(point_c[1],point_c[0])
-ax2.scatter(point_d[1],point_d[0])
+ax2.scatter(point_a[1],point_a[0],label = "point a") 
+ax2.scatter(point_b[1],point_b[0],label = "point b")
+ax2.scatter(point_c[1],point_c[0],label = "point c")
+ax2.scatter(point_d[1],point_d[0],label = "point d")
+plt.legend()
 # plt.show()
 
 print("sum_vort", sum_vort)
@@ -215,55 +216,89 @@ def path_integral_velocity97(
 
     return Circulation
 
-u, v = oj.importData("G:/AftificialVortex/Hamel-Oseen Vortex/PIVlab.mat")
-Hamel_Osseen = oj.sum_Vorticity(u, v)
-V_mag = np.sqrt((np.square(u) + np.square(v)))
-Hamel_Osseen_Circ = path_integral_velocity97(u[0,:,:], v[0,:,:])
-print("Hamel_Osseen_Circ: ",  int(Hamel_Osseen_Circ))
-print("Hamel_Osseen_sum_vort: ",  int(Hamel_Osseen[0]))
+# u, v = oj.importData("G:/AftificialVortex/Hamel-Oseen Vortex/PIVlab.mat")
+# Hamel_Osseen = oj.sum_Vorticity(u, v)
+# V_mag = np.sqrt((np.square(u) + np.square(v)))
+# Hamel_Osseen_Circ = path_integral_velocity97(u[0,:,:], v[0,:,:])
+# print("Hamel_Osseen_Circ: ",  int(Hamel_Osseen_Circ))
+# print("Hamel_Osseen_sum_vort: ",  int(Hamel_Osseen[0]))
 
-f3, ax3 = plt.subplots()
-ax3.quiver(u[0,:,:],v[0,:,:])
-ax3.scatter(point_a[1],point_a[0], label = "Point a") 
-ax3.scatter(point_b[1],point_b[0], label = "Point b")
-ax3.scatter(point_c[1],point_c[0], label = "Point c")
-ax3.scatter(point_d[1],point_d[0], label = "Point d")
-plt.legend()
+# f3, ax3 = plt.subplots()
+# ax3.quiver(u[0,:,:],v[0,:,:])
+# ax3.scatter(point_a[1],point_a[0], label = "Point a") 
+# ax3.scatter(point_b[1],point_b[0], label = "Point b")
+# ax3.scatter(point_c[1],point_c[0], label = "Point c")
+# ax3.scatter(point_d[1],point_d[0], label = "Point d")
+# plt.legend()
+# # plt.show()
+
+# Hamel_Osseen_Vort, gauss = oj.calculate_vorticity(u, v)
+
+
+# f4, ax4 = plt.subplots()
+# ax4.contourf(gauss[0,:,:])
+# ax4.scatter(point_a[1],point_a[0], label = "Point a") 
+# ax4.scatter(point_b[1],point_b[0], label = "Point b")
+# ax4.scatter(point_c[1],point_c[0], label = "Point c")
+# ax4.scatter(point_d[1],point_d[0], label = "Point d")
+# plt.legend()
+
+# print(int(Hamel_Osseen_Vort[0, int(Hamel_Osseen_Vort.shape[1]/2), int(Hamel_Osseen_Vort.shape[2]/2)]))
+
+# f5, ax5 = plt.subplots()
+# plt.title("Velocity Magnitude Profile midline")
+# ax5.plot(V_mag[0,50,:])
+# plt.legend()
+
+
+# f6, ax6 = plt.subplots()
+# plt.title("Vorticity Magnitude Profile midline")
+# ax6.plot(Hamel_Osseen_Vort[0, int(Hamel_Osseen_Vort.shape[1]/2), :])
+# plt.legend()
+
+
+# f7, ax7 = plt.subplots()
+# plt.title("v velocity Profile midline in y")
+# ax7.plot(v[0, int(Hamel_Osseen_Vort.shape[1]/2), :])
+# plt.legend()
+
+
+# f8, ax8 = plt.subplots()
+# plt.title("u velocity Profile midline in x")
+# ax8.plot(u[0, :, int(Hamel_Osseen_Vort.shape[2]/2)])
+# plt.legend()
 # plt.show()
 
-Hamel_Osseen_Vort, gauss = oj.calculate_vorticity(u, v)
 
+#### line integral of tangent velocity #####
+def path_integral_Tan_velocity(
+        u, v
+):
+    Vel = np.sqrt(np.square(U)+np.square(V))
 
-f4, ax4 = plt.subplots()
-ax4.contourf(gauss[0,:,:])
-ax4.scatter(point_a[1],point_a[0], label = "Point a") 
-ax4.scatter(point_b[1],point_b[0], label = "Point b")
-ax4.scatter(point_c[1],point_c[0], label = "Point c")
-ax4.scatter(point_d[1],point_d[0], label = "Point d")
-plt.legend()
+    point_a = [25, 25]
+    point_b = [25, 75]
+    point_c = [75, 75]
+    point_d = [75, 25]
 
-print(int(Hamel_Osseen_Vort[0, int(Hamel_Osseen_Vort.shape[1]/2), int(Hamel_Osseen_Vort.shape[2]/2)]))
+    # path1 = #(25,25) - (75,25) in x
+    # path2 = #(75,25) - (75,75) in y
+    # path3 = #(75,75) - (25,75) in -x
+    # path4 = #(25,75) - (25,25) in -y
 
-f5, ax5 = plt.subplots()
-plt.title("Velocity Magnitude Profile midline")
-ax5.plot(V_mag[0,50,:])
-plt.legend()
+    sumpath1 = sum(abs(u[24, 24:76]))
+    sumpath2 = sum(abs(v[24:76, 76]))
+    sumpath3 = sum(abs(u[76, 76:24]))
+    sumpath4 = sum(abs(v[76:24, 24]))
 
+    Circulation = abs(sumpath1) + abs(sumpath2) + abs(sumpath3) + abs(sumpath4)
 
-f6, ax6 = plt.subplots()
-plt.title("Vorticity Magnitude Profile midline")
-ax6.plot(Hamel_Osseen_Vort[0, int(Hamel_Osseen_Vort.shape[1]/2), :])
-plt.legend()
+    return Circulation
 
+Tan_circ = path_integral_Tan_velocity(U, V)
+print("Tangent Velocity ", Tan_circ)
 
-f7, ax7 = plt.subplots()
-plt.title("v velocity Profile midline in y")
-ax7.plot(v[0, int(Hamel_Osseen_Vort.shape[1]/2), :])
-plt.legend()
-
-
-f8, ax8 = plt.subplots()
-plt.title("u velocity Profile midline in x")
-ax8.plot(u[0, :, int(Hamel_Osseen_Vort.shape[2]/2)])
-plt.legend()
+f3, ax3 = plt.subplots(sharex=True, sharey=True)
+ax3.quiver(X, Y, U, (0.01*V), units='xy', pivot = 'middle', scale=scale)
+ax3.contour((5*(Y**2))+(0.5*(X**2)))
 plt.show()
