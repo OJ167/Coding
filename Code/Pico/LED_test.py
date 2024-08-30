bcc5bc78e4963aff3cdf7fec735f2768587a2894
OJ167 <olliejackson@hotmail.co.uk>
2024-08-30 13:17:15 +0100

    pico_test_30_08_2024

--- Code/Pico/LED_test.py
+++ Code/Pico/LED_test.py
@@ -1,3 +1,10 @@
 import machine
+import time
+
 led_onboard = machine.Pin(25, machine.Pin.OUT)
-led_onboard.value(0)
+
+while True:
+    led_onboard.value(0)
+    time.sleep_ms(1000)
+    led_onboard.value(1)
+    time.sleep_ms(1000)
