e4662ae8867951f18028369508d396294a074e85
OJ167 <olliejackson@hotmail.co.uk>
2025-01-01 01:50:37 +0000

    update_31_12_2024

--- Code/Python/Thesis/Rossby_Ekman_Re_calculator.py
+++ Code/Python/Thesis/Rossby_Ekman_Re_calculator.py
@@ -1,20 +1,20 @@
 import sys
 #####Import Ollie Tools
-dirPath = "C:/Coding"
-sys.path.insert(0, dirPath)
-import OllieTools as oj
-print(dirPath)
-
-
-####Import Ollie Tools MAC
-# dirPath = "/Users/olliejackson/Coding"
+# dirPath = "C:/Coding"
 # sys.path.insert(0, dirPath)
 # import OllieTools as oj
 # print(dirPath)
 
-u0 = 0.2
+
+####Import Ollie Tools MAC
+dirPath = "/Users/olliejackson/Coding"
+sys.path.insert(0, dirPath)
+import OllieTools as oj
+print(dirPath)
+
+u0 = 0.1
 l0 = 0.1
-omega = 3
+omega = 12
 
 Re = oj.Rej(u0, l0)
 print('Re_j = ', Re)
